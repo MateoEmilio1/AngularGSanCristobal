8e76809cd9f09c0818425777cadff5cc19a633dd
Mateo Emilio <mateoyastor60@gmail.com>
2022-09-07 15:45:40 -0300

    first commit

---
+++ Ejercicios/Ejercicio 1.1 .js
@@ -0,0 +1,10 @@
+let pi = 3.14159;
+let tau = pi * 2;
+
+console.log('[Ejercicio 1.1]', `${tau} es ${pi} veces el dos.`);
+
+
+let pi2 = '3.14159';
+let tau2 = pi * 2;
+
+console.log('[Ejercicio 1.1]', `${tau2} es ${pi2} veces el dos.`);
