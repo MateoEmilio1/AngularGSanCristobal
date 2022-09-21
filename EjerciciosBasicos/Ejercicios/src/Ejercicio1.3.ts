50295a539f297c404584e376373d518bc84c2775
Mateo Emilio <mateoyastor60@gmail.com>
2022-09-21 12:50:05 -0300

    actualizacion ejercicios basicos

---
+++ EjerciciosBasicos/Ejercicios/src/Ejercicio1.3.ts
@@ -0,0 +1,11 @@
+let esPablo: boolean;
+
+esPablo = true;
+
+console.log('[Ejercicio 1.3]', `${esPablo ? 'Oh, hola Pablo' : 'Quien sos vos?'}`);
+
+esPablo = false;
+
+console.log('[Ejercicio 1.3]', `${esPablo ? 'Oh, hola Pablo' : 'Quien sos vos?'}`);
+
+
