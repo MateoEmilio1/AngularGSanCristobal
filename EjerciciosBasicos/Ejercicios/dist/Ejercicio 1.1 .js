53612512c9e6ae21e707be9c832c0e312b528db5
Mateo Emilio <mateoyastor60@gmail.com>
2022-09-21 11:39:11 -0300

    Act ejercicios

---
+++ EjerciciosBasicos/Ejercicios/dist/Ejercicio 1.1 .js
@@ -0,0 +1,7 @@
+"use strict";
+var pi = 3.14159;
+var tau = pi * 2;
+console.log('[Ejercicio 1.1]', tau + " es " + pi + " veces el dos.");
+var pi2 = '3.14159';
+var tau2 = pi * 2;
+console.log('[Ejercicio 1.1]', tau2 + " es " + pi2 + " veces el dos.");
---
+++ EjerciciosBasicos/Ejercicios/dist/Ejercicio 1.2 .js
@@ -0,0 +1,5 @@
+"use strict";
+var torta;
+torta = 'arandanos';
+console.log('[Ejercicio 1.2]', "Me gusta comer torta con sabor a " + torta + ".");
+//test
