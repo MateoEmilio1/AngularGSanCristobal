2340acb5719bb92f95450d01897c810a7e7a5727
Mateo Emilio <mateoyastor60@gmail.com>
2022-09-21 17:07:58 -0300

    act ej

---
+++ EjerciciosBasicos/EjerciciosTypescript-Tipos/src/Ejercicio4.0.ts
@@ -0,0 +1,26 @@
+function doStuff(value: any): void {
+    if (typeof value === 'string') {
+
+     console.log(value.toUpperCase().split('').join(' '));
+
+    } else if (typeof value === 'number') {
+
+     console.log(value.toPrecision(5));
+
+    }
+    
+     value; // coloque el cursor sobre `valor` aqui
+     
+     //devuelve value (doStuff: void)
+}
+    
+doStuff(2);
+doStuff(22);
+doStuff(222);
+console.log("Que pasa si paso un numero con + de 5 cifras? ");
+doStuff(222222);
+doStuff('hello');
+doStuff(true);
+doStuff({});
+    
+console.log('[Ejercicio 4.1]');
